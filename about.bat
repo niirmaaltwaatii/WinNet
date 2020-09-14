fc28f5956f838bd71bc27bdbfc56a9a7fc2bd04b
niirmaaltwaatii <twaatiiniirmaal@gmail.com>
2020-09-14 15:32:32 +0545

    Initial Commit

---
+++ about.bat
@@ -0,0 +1,14 @@
+:: https://github.com/niirmaaltwaatii/WinFI
+
+echo {
+echo  ----------------------------------------------------
+echo [ Tool : WinFi                                       ]
+echo [ Version : 1                                        ]
+echo [ Creator : Niirmaal Twaatii                         ]
+echo [ GitHub : https://github.com/niirmaaltwaatii/WinFI  ]
+echo  ----------------------------------------------------
+echo }
+
+pause
+
+winfi.bat
---
+++ showall.bat
@@ -0,0 +1,9 @@
+:: https://github.com/niirmaaltwaatii/WinFI
+
+echo {
+netsh wlan show profiles
+echo }
+
+pause
+
+winfi.bat
