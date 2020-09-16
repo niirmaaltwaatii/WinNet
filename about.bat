502be8c88bca1ceaa87c0326ee943bdb9fe603e3
niirmaaltwaatii <twaatiiniirmaal@gmail.com>
2020-09-16 06:37:54 +0545

    tool name changed

--- about.bat
+++ about.bat
@@ -1,14 +1,14 @@
-:: https://github.com/niirmaaltwaatii/WinFI
-
+:: https://github.com/niirmaaltwaatii/WinNet
+REM N11rm44L 7w44711
 echo {
 echo  ----------------------------------------------------
-echo [ Tool : WinFi                                       ]
+echo [ Tool : WinNet                                      ]
 echo [ Version : 1                                        ]
 echo [ Creator : Niirmaal Twaatii                         ]
-echo [ GitHub : https://github.com/niirmaaltwaatii/WinFI  ]
+echo [ GitHub : https://github.com/niirmaaltwaatii/WinNet ]
 echo  ----------------------------------------------------
 echo }
 
 pause
 
-winfi.bat
+winnet.bat
