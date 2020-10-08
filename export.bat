9d80a9355a475acbff89aa20673f6964508661d1
niirmaaltwaatii <twaatiiniirmaal@gmail.com>
2020-10-09 04:56:27 +0545

    Bug Fixes

--- export.bat
+++ export.bat
@@ -3,8 +3,17 @@ REM BY N11rm44L 7w44711
 
 echo {
 set /p ex="ALL or Specific (a/s) :"
-if %ex%==a (echo Exporting All Network Including Passwords ........ & netsh wlan export profile key=clear folder=exports)
-if %ex%==s (set /p exn="Name : " & netsh wlan export profile name=%exn% key=clear folder=exports)
+if %ex%==a (echo Exporting All Network Including Passwords ........ && goto all)
+if %ex%==s (set /p exn="Name : " && goto specific)
+
+:all
+netsh wlan export profile key=clear folder=exports
+goto end
+
+:specific
+netsh wlan export profile name=%exn% key=clear folder=exports
+
+:end
 
 echo }
 
